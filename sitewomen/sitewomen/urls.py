e65e61684179f933f4944c62dc36f1a7a2983e5d
wl9pa <wl9pa-92-92@mail.ru>
2024-12-02 19:24:11 +0300

    second

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sitewoman project.
+URL configuration for sitewomen project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from women import views
+from women.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('women.urls')),
 ]
+
+handler404 = page_not_found
