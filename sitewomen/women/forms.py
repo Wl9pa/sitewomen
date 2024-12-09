26b5ec32491be679aad0a8c3e64a60f4bc18607b
wl9pa <wl9pa-92-92@mail.ru>
2024-12-09 20:25:40 +0300

    Формы связанные с моделью, загрузка файлов на сервер

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 
-from .models import Category, Husband, TagPost
+from .models import Category, Husband, TagPost, Women
 
 
 # @deconstructible
@@ -20,20 +20,18 @@ from .models import Category, Husband, TagPost
 #             raise ValidationError(self.message, code=self.code)
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, min_length=3, label='Заголовок',
-                            widget=forms.TextInput(attrs={'class': 'form-input'}),
-                            error_messages={
-                                'min_length': 'Слишком короткий заголовок, минимум 3 символа',
-                                'required': 'Заполните заголовок'
-                            })
-    # slug = forms.SlugField(max_length=255, label='URL')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент")
-    is_published = forms.BooleanField(required=False, initial=True, label='Статус')
+class AddPostForm(forms.ModelForm):  # Форма связанная с моделью Women
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Категория не выбрана', label='Категории')
-    tags = forms.ModelMultipleChoiceField(queryset=TagPost.objects.all(), required=False, label='Теги')
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), empty_label='Не замужем', required=False,
                                      label='Муж')
+
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat', 'husband', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 50, 'rows': 5})
+        }
 
     def clean_title(self):  # Для частной проверки
         title = self.cleaned_data['title']
@@ -41,3 +39,23 @@ class AddPostForm(forms.Form):
 
         if not (set(title) <= set(ALLOWED_CHARS)):
             raise ValidationError("Должны присутствовать только русские символы, дефис и пробел.")
+        elif len(title) > 50:
+            raise ValidationError("Длина превышает 50 символов")
+        return title
+
+
+    # title = forms.CharField(max_length=255, min_length=3, label='Заголовок',
+    #                         widget=forms.TextInput(attrs={'class': 'form-input'}),
+    #                         error_messages={
+    #                             'min_length': 'Слишком короткий заголовок, минимум 3 символа',
+    #                             'required': 'Заполните заголовок'
+    #                         })
+    # # slug = forms.SlugField(max_length=255, label='URL')
+    # content = forms.CharField(widget=, required=False, label="Контент")
+    # is_published = forms.BooleanField(required=False, initial=True, label='Статус')
+    #
+    # tags = forms.ModelMultipleChoiceField(queryset=TagPost.objects.all(), required=False, label='Теги')
+    #
+
+class UploadFileForm(forms.Form):
+    file = forms.ImageField(label='Файл')
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.text import slugify
 from unidecode import unidecode
-from .forms import AddPostForm
-from .models import Women, Category, TagPost
+from .forms import AddPostForm, UploadFileForm
+from .models import Women, Category, TagPost, UploadFiles
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -24,8 +24,24 @@ def index(request):
     return render(request, 'women/index.html', context=data)
 
 
+# def handle_uploaded_file(f):
+#     with open(f"uploads/{f.name}", "wb+") as destination:
+#         for chunk in f.chunks():
+#             destination.write(chunk)
+
+
 def about(request):
-    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
+    if request.method == 'POST':
+        # handle_uploaded_file(request.FILES['file_upload'])
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # handle_uploaded_file(form.cleaned_data['file'])
+            fp = UploadFiles(file=form.cleaned_data['file'])
+            fp.save()
+    else:
+        form = UploadFileForm()
+    return render(request, 'women/about.html',
+                  {'title': 'О сайте', 'menu': menu, 'form': form})
 
 
 def show_post(request, post_slug):
@@ -45,17 +61,20 @@ def custom_slugify(value):
 
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             # print(form.cleaned_data)
-            try:
-                form.cleaned_data['slug'] = custom_slugify(form.cleaned_data['title'])
-                tags = form.cleaned_data.pop('tags')
-                new_women = Women.objects.create(**form.cleaned_data)
-                new_women.tags.set(tags)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            # try:
+            #     form.cleaned_data['slug'] = custom_slugify(form.cleaned_data['title'])
+            #     tags = form.cleaned_data.pop('tags')
+            #     new_women = Women.objects.create(**form.cleaned_data)
+            #     new_women.tags.set(tags)
+            #     return redirect('home')
+            # except:
+            #     form.add_error(None, 'Ошибка добавления поста')
+            # form.cleaned_data['slug'] = custom_slugify(form.cleaned_data['title'])
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
 
