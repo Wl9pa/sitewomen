727f6aaacafcd8848f82a4982970d96ef7ee3e2f
wl9pa <wl9pa-92-92@mail.ru>
2024-12-17 18:28:48 +0300

    Расширение модели User. Класс AbstractUser

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -143,16 +143,20 @@ AUTHENTICATION_BACKENDS = [
     'users.authentication.EmailAuthBackend',
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#
+# EMAIL_HOST_PASSWORD = "zbuekakprdsqnleg"
+#
+# EMAIL_HOST = "smtp.yandex.ru"
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER =
+# EMAIL_HOST_PASSWORD =
+# EMAIL_USE_TLS = True
+#
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# SERVER_EMAIL = EMAIL_HOST_USER
+# EMAIL_ADMIN = EMAIL_HOST_USER
 
-EMAIL_HOST_PASSWORD = "zbuekakprdsqnleg"
+AUTH_USER_MODEL = 'users.User'
 
-EMAIL_HOST = "smtp.yandex.ru"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "wl9pa-92-92@yandex.ru"
-EMAIL_HOST_PASSWORD =
-EMAIL_USE_TLS = True
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-SERVER_EMAIL = EMAIL_HOST_USER
-EMAIL_ADMIN = EMAIL_HOST_USER
+DEFAULT_USER_IMAGE = MEDIA_URL + 'users/default.png'
