f0c44cf57dcbdba94493e7f5f81a08578e25cae9
wl9pa <wl9pa-92-92@mail.ru>
2024-12-18 22:51:33 +0300

    f

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -151,10 +151,9 @@ AUTHENTICATION_BACKENDS = [
 
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 #
-# EMAIL_HOST_PASSWORD = "zbuekakprdsqnleg"
 #
-# EMAIL_HOST = "smtp.yandex.ru"
-# EMAIL_PORT = 587
+# EMAIL_HOST = env.str('EMAIL_HOST')
+# EMAIL_PORT = env.int('EMAIL_PORT')
 # EMAIL_HOST_USER =
 # EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
 # EMAIL_USE_TLS = True
